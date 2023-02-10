f81031de25ebe95100747de6e9020b584b3bbdd7
Firas Sediri <sfiras1111@gmail.com>
2023-02-10 20:05:49 +0100

    Update DeathBarrier.cs

--- Assets/Scripts/DeathBarrier.cs
+++ Assets/Scripts/DeathBarrier.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
+    // there are death barriers between the gaps of the stage 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
